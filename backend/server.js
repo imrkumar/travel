994e28da5c30907c6e1c8b2977e62658db7dca24
Rahul Kumar <rahulkumar981417@gmail.com>
2025-06-02 20:05:10 +0530

    did some chagnes for vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -6,32 +6,32 @@ const authRoutes = require("./src/routes/authRoutes");
 const blogRoutes = require("./src/routes/blogRoutes");
 const trekRoutes = require("./src/routes/trek.routes");
 const contactRoutes = require("./src/routes/contact.routes");
+
 const app = express();
 dbConnect();
-
-require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
+// Default test route
+app.get("/", (req, res) => {
+  res.send("API is working.");
+});
+
 app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-  });
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 app.use("/api", authRoutes);
 app.use("/api/blog", blogRoutes);
 app.use("/api/trek", trekRoutes);
-app.use("/api/contact",contactRoutes);
+app.use("/api/contact", contactRoutes);
 
 app.use((err, req, res, next) => {
-    console.error("Global error handler:", err);
-    res.status(500).json({ error: err.message || err.toString() });
-  });
-const PORT = process.env.PORT || 9000;
-app.listen(PORT, (error) => {
-  if (!error) {
-    console.log(`Server is running on port ${PORT}`);
-  }
+  console.error("Global error handler:", err);
+  res.status(500).json({ error: err.message || err.toString() });
 });
 
+//Export the app for Vercel
+module.exports = app;
